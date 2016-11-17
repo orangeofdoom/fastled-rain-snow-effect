de0dc09398cba1fb045678c70b6e2f45c02126e4
orangeofdoom <jeffjerzy@gmail.com>
2016-11-16 19:31:22 -0500

    more changes base off Google+ comments

--- src/new-rain.ino
+++ src/new-rain.ino
@@ -1,8 +1,14 @@
+//this is a program I wrote far a light box istallation in my gloomy ass bedroom
+//
+
+
+
+
 #include "FastLED.h"           	// This sketch uses the FastLED library
 
-#define DATA_PIN 6    		   	// Data connected to Digital Pin 2 on the Arduino
+#define DATA_PIN 6    		   	// Data connected to Digital Pin 6 on the Arduino
 
-#define LED_TYPE WS2811      	// This LED strip uses the WS2801 chipset.
+#define LED_TYPE WS2811      	//this is an array of 40 in an 8X5 pattern
 #define NUM_LEDS 40
 
 CRGB leds[NUM_LEDS];
@@ -14,13 +20,15 @@ void setup() {
 }
 
 void loop() {
-   // Move a single white led
+   // Move a single white led in its own row
    for(int Row1 = 0; Row1 < 8; Row1 = Row1 + 1) for(int row2 = 8; row2 <16; row2 = row2 +1) {
-       leds[Row1] = CRGB::White; leds[row2] = CRGB :: White; FastLED.show();
-      delay(100);
-    leds[Row1] = CRGB ::Black;
-    leds[row2] = CRGB ::Black;
-
+       leds[Row1] = CRGB::White; leds[row2] = CRGB :: White;}
+     delay(1000);
       FastLED.show();
-}
+//same as above but now it trns them off 
+   for(int Row1 = 0; Row1 < 8; Row1 = Row1 + 1) for(int row2 = 8; row2 <16; row2 = row2 +1)
+    {leds[Row1] = CRGB ::Black; leds[row2] = CRGB ::Black;}
+    delay(1000);
+      FastLED.show();
+
 }
